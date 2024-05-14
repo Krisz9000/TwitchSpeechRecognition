75d29cd910024d902c89e7c627d2fff603972634
Krisztián Kafka <krisztian.kafka@tum.de>
2024-05-14 20:22:24 +0200

    refactoring and added Chat capture

--- src/main.py
+++ src/main.py
@@ -1,25 +1,45 @@
 # imports
 import speech_recognition as sr
 
-# setup Recogniser
+# setup Recogniser and sources
 r = sr.Recognizer()
+# must be customized for all users / or made smhow automatic
+mic = sr.Microphone(device_index=1)
+dc = sr.Microphone(device_index=4)
 
 
-def main():
-    print("Hello World")
+def listenOnMic():
     # start listening on Mic
-    with sr.Microphone() as source:
+    with mic as source:
         print("Listening")
         audio = r.listen(source)
+        googleInterpret(audio)
 
+
+def googleInterpret(audio):
     # Google Interpret output
     try:
-        print("Google Speech Recognition thinks you said " + r.recognize_google(audio, language='hu'))
+        print("Booglidú szerint ezt mondtad: " + r.recognize_google(audio, language='hu', pfilter=0))
     except sr.UnknownValueError:
-        print("Google Speech Recognition could not understand audio")
+        print("Booglidú nem érti mit vakerálsz..")
     except sr.RequestError as e:
-        print("Could not request results from Google Speech Recognition service; {0}".format(e))
+        print("Booglidú nem volt elérhető; {0}".format(e))
+
+
+def listenOnChat():
+    # start listening on Chat
+    with dc as source:
+        print("Listening")
+        audio = r.listen(source)
+        googleInterpret(audio)
 
 
 if __name__ == "__main__":
-    main()
+    # list audio devices
+    print(sr.Microphone.list_microphone_names())
+
+    # loop
+    while True:
+        listenOnMic()
+        # TODO test chat input
+        # listenOnChat()
