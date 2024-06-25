238a2fdbd8bc893cbba5079305a63f4de0a69a92
Krisztián Kafka <krisztian.kafka@tum.de>
2024-06-26 00:26:21 +0200

    Added proper TODOs, back-end for default mic done, needs test cases tho

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 # imports
 import time
 
+# Libraries for successful interpretation, ...and of the Microphone
 import speech_recognition as sr
 from speech_recognition import AudioData
 
@@ -10,6 +11,7 @@ r.energy_threshold = 600
 r.dynamic_energy_threshold = True
 # must be customized for all users / or made smhow automatic
 global mic
+# might not work if not default device, TODO make a front-end for selecting it
 mic = sr.Microphone(device_index=1)
 global dc
 global cachedAudioData
@@ -20,6 +22,7 @@ with mic as source:
 print("Adjustment done")
 
 
+# Listening for the default input device (prob a mic), and automatically interpreting it in small segments
 def listenOnMic():
     # start listening on Mic
     with mic as Mic:
@@ -27,11 +30,12 @@ def listenOnMic():
         googleInterpret(r, audio)
 
 
-# TODO caching bg audio
+# TODO caching bgrnd audio for simultaneous processing and listening on different threads
 def callbackForBgListen(recognizer, audio):
     cachedAudioData = audio
 
 
+# Function for writing a real-time audio output into a single, fix-ended audio file
 def recordToFile(input):
     with input as source:
         audio = r.listen(source)
@@ -40,6 +44,7 @@ def recordToFile(input):
         f.write(audio.get_wav_data())
 
 
+# Function for passing an audio data to the Google Speech Interpreter
 def googleInterpret(recognizer, audiodata):
     # Google Interpret output
     try:
@@ -50,8 +55,11 @@ def googleInterpret(recognizer, audiodata):
         print("Booglidú nem volt elérhető; {0}".format(e))
 
 
+# listening for Discord input || only works if VB/Audio driver configured properly, TODO bundle VBAudio
+# TODO make front-end for selecting and configuring VBAudio Driver
 def listenOnChat():
     # start listening on Chat
+    # Only works if the stars and Gods want it so....  TODO need a menu where properly selectable
     dc = sr.Microphone(device_index=4)
     with dc as source:
         audio = r.listen(source=source, timeout=None, phrase_time_limit=6, snowboy_configuration=None)
@@ -65,13 +73,17 @@ def listenOnChat():
             print("Booglidú nem volt elérhető; {0}".format(e))
 
 
+# TODO Not comprehensible! Cleanup, too many angles for solutions right now.
 if __name__ == "__main__":
     # list audio devices
     print(sr.Microphone.list_microphone_names())
     # set up cache
+    # Trying to set up a multithreaded solution, where one thread is actively interpreting what a
+    # second thread is actively listening to, that`s what the cache is for
     lastInterpretedAudioData: AudioData = None
     cachedAudioData: AudioData = None
     print("Listening In Background...")
+    # setting up shut-down function || if called, stops bg listening
     stop_listening = r.listen_in_background(mic, callbackForBgListen, 8)
     # loop
     while True:
